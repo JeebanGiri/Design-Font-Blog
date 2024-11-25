f1cc7a0234bdcb39fd63a2409c32fe4c44083de3
Jeeban Giri <jeebangiri942@gmail.com>
2024-11-25 11:05:11 +0545

    refactor the code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
-const Home = ()=>{
-    return(
-        <>
-            <h2>Best font for web design</h2>
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <>
+      <h2>Best font for web design</h2>
+    </>
+  );
+};
 export default Home;
